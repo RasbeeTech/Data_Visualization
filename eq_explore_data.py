873f80c2e145137280585e625b0493db859271ed
MykalB081 <mykalb081@gmail.com>
2021-04-29 16:19:40 -0400

    Extract data
    - Make a list of all earthquakes
    - extract magnitude and location data

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -5,6 +5,20 @@ filename = 'data/eq_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
-readable_file = 'data/readable_eq_data.json'
-with open(readable_file, 'w') as f:
-    json.dump(all_eq_data, f, indent=4)
+# Make a list of all earthquakes.
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
+
+# Extract magnitude and location data.
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])
