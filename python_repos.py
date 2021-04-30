304a44d29d2f04ecbc277a4aca90d9031bd01bc5
MykalB081 <mykalb081@gmail.com>
2021-04-30 10:02:56 -0400

    Using APIs 2
    - Show all repo details

--- python_repos.py
+++ python_repos.py
@@ -17,13 +17,12 @@ print(f"Total repositories: {response_dict['total_count']}")
 repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 
-# Examine first repo.
-repo_dict = repo_dicts[0]
-print("\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+# Examine all repos.
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
